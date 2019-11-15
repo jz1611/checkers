0019cfe7844bb7feaaf0be94d955b12448c5a7e1
Jeff Zivkovic <jzivkovic1611@gmail.com>
2019-11-14 17:01:29 -0700

    Stuck on changing piece colors

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 var board = [];
+var topPieces = 12;
+var bottomPieces = 12;
 var topWins = 0;
 var bottomWins = 0;
 var topTokensGone = 0;
 var bottomTokensGone = 0;
+var topColor = '';
+var bottomColor = '';
 const resetBoardButton = document.getElementById('board-reset');
 const resetScoreButton = document.getElementById('score-reset');
 
+resetBoardButton.addEventListener('click', resetBoard);
+resetScoreButton.addEventListener('click', resetScore);
 
 function resetBoard(){
     board = [
@@ -23,11 +29,59 @@ function resetBoard(){
         for (let j = 0; j < board[i].length; j++) {
             let square = document.getElementById(`${i}-${j}`);
             square.innerHTML = board[i][j];
+            square.addEventListener('click', selectToken);
+
+            // if (square.innerHTML === '&#9679;') {
+            //     square.style.color = getTopColor();
+            // }
+    
+            // if (square.innerHTML === '&#9675;') {
+            //     square.style.color = getBottomColor();
+            // }
         }
     }
+
+    console.dir(document.getElementById('board'))
 
     topTokensGone = 0;
     bottomTokensGone = 0;
 }
 
-resetBoardButton.addEventListener('click', resetBoard);
+function resetScore() {
+    topWins = 0;
+    bottomWins = 0;
+}
+
+function selectToken() {
+    if (this.innerHTML === '') {
+        return;
+    } else {
+        this.style.border = "solid red 5px"
+        moveToken();
+        this.style.border = "";
+    }
+}
+
+function moveToken() {
+
+}
+
+// function getTopColor() {
+//     let colors = document.getElementsByName('top-color');
+
+//     for (let i = 0; i < colors.length; i++) {
+//         if(colors[i].checked) {
+//             return colors[i];
+//         }
+//     }
+// }
+
+// function getBottomColor() {
+//     let colors = document.getElementsByName('bottom-color');
+
+//     for (let i = 0; i < colors.length; i++) {
+//         if(colors[i].checked) {
+//             return colors[i];
+//         }
+//     }
+// }
